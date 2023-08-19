89a682915970f64cceee1a3f9370f64c701f7d83
Attapon Jeamjumroensuk <attapon.j@arise.tech>
2023-08-19 15:58:09 +0700

    First commit

--- k6-scripts/sample.js
+++ k6-scripts/sample.js
@@ -1,23 +1,10 @@
 import http from "k6/http";
 import { check, sleep } from "k6";
 
-export let options = {
-  stages: [
-    // Ramp-up from 1 to 5 virtual users (VUs) in 5s
-    { duration: "5s", target: 5 },
 
-    // Stay at rest on 5 VUs for 10s
-    { duration: "10s", target: 5 },
-
-    // Ramp-down from 5 to 0 VUs for 5s
-    { duration: "5s", target: 0 },
-  ],
-};
 
 export default function () {
-  const response = http.get("<target API>", {
-    headers: { Accepts: "application/json" },
-  });
+  const response = http.get("[MY_API]");
   check(response, { "status is 200": (r) => r.status === 200 });
-  sleep(0.3);
+  sleep(0.5);
 }
